f473488061bcfc2f0181dfe3780675803e8412c2
liuzedong <liuzedong@kanzhun.com>
2025-12-02 10:30:14 +0800

    Initial Commit

---
+++ GuopazIOT/GuopazIOTApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuopazIOTApp.swift
+//  GuopazIOT
+//
+//  Created by liuzedong on 2025/12/2.
+//
+
+import SwiftUI
+
+@main
+struct GuopazIOTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
